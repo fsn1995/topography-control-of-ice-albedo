dc6552f281e644bb99f6f1459ad90fcf61266ab9
fsn1995 <fsn.1995@gmail.com>
2022-12-12 16:18:51 +0100

    qgis topomap

--- topoDarkIceML.m
+++ topoDarkIceML.m
@@ -1,22 +1,21 @@
 
 
 %% load data
-% topodf = readtable("H:\AU\topography\basin\SW_annual.csv");
-topodf = readtable("/data/shunan/data/topography/basin/SE_annual.csv");
-
-% topodf.distance = topodf.dist / 1000;
+topodf = readtable("H:\AU\topography\basin\SW_annual.csv");
+% topodf = readtable("/data/shunan/data/topography/basin/SE_annual.csv");
 topodf.iceclass = repmat("bare ice", length(topodf.albedo) , 1);
 index = topodf.albedo < 0.45;
 topodf.iceclass(index) = repmat("dark ice", sum(index) , 1);
 topodf.distance = topodf.dist / 1000;
 %% prepare input
-% index = topodf.distance>=6.02642477064999;
-% topodf = topodf(index, :);
+index = topodf.distance<9.57041446875;
+topodf = topodf(index, :);
 
 slope = normalize(topodf.slope, 'range', [0 1]);
 aspect = normalize(topodf.aspect, 'range', [0 1]);
 elevation = normalize(topodf.elevation, 'range', [0 1]);
-distance = normalize(topodf.distance, 'range', [0 1]);
+% distance = normalize(topodf.distance, 'range', [0 1]);
+% duration = normalize(topodf.duration, 'range', [0 1]);
 
 % %% pca analysis
 % [coeff,score,latent,tsquared,explained,mu] = pca([elevation aspect slope]);
@@ -28,7 +27,7 @@ distance = normalize(topodf.distance, 'range', [0 1]);
 %% split data 
 
 % df = table(gpuArray(slope), gpuArray(aspect), gpuArray(elevation));
-df = table(slope, aspect, elevation, distance);
+df = table(slope, aspect, elevation);
 % df = table(score(:,1), score(:,2));
 
 cvpt = cvpartition(topodf.iceclass, "HoldOut", 0.3);
@@ -45,101 +44,50 @@ testLabel = topodf.iceclass(testId);
 
 
 %% training model
-% options = struct("Optimizer","asha", "UseParallel",true);
-% [Mdl,OptimizationResults] = fitcauto(trainData, trainLabel, "Learners","auto",...
-%     "HyperparameterOptimizationOptions",options);
-% 
-% mdlPred = string(predict(Mdl, testData));
-% mdlLoss = loss(Mdl, testData, testLabel);
-% fprintf("model loss rate is: %.4f \n", mdlLoss);
-% figure;
-% confusionchart(testLabel, mdlPred);
+% SW 0.2945 0.3006
+t = templateTree("Reproducible", true, "Surrogate", "on", "MinLeafSize", 30);
+mdl = fitcensemble(trainData, trainLabel,"Method", "Bag",...
+     "Learners", t, "NumLearningCycles", 28);
 
-%% knn
-% fprintf("knn \n")
-% mdl = fitcknn(trainData, trainLabel,"OptimizeHyperparameters","auto");
-% 
-% mdlPred = string(predict(mdl, testData));
-% mdlLoss = loss(mdl, testData, testLabel);
-% fprintf("model loss rate is: %.4f \n", mdlLoss);
-% figure;
-% confusionchart(testLabel, mdlPred);
-% title("cknn");
-% 
-% %% decision tree
-% fprintf("tree \n")
-% mdl = fitctree(trainData, trainLabel, 'OptimizeHyperparameters','auto');
-% 
-% mdlPred = string(predict(mdl, testData));
-% mdlLoss = loss(mdl, testData, testLabel);
-% fprintf("model loss rate is: %.4f \n", mdlLoss);
-% figure;
-% confusionchart(testLabel, mdlPred);
-% title("tree");
-% 
-% %% Naïve Bayes
-% fprintf("Naïve Bayes \n")
-% mdl = fitcnb(trainData, trainLabel, "OptimizeHyperparameters","auto");
-% 
-% mdlPred = string(predict(mdl, testData));
-% mdlLoss = loss(mdl, testData, testLabel);
-% fprintf("model loss rate is: %.4f \n", mdlLoss);
-% figure;
-% confusionchart(testLabel, mdlPred);
-% title("Naïve Bayes");
-% 
-%% neural network
-% fprintf("neural network \n")
-% mdl = fitcnet(trainData, trainLabel,"OptimizeHyperparameters","auto"); %"LayerSizes",[35 20]
-% 
-% mdlPred = string(predict(mdl, testData));
-% mdlLoss = loss(mdl, testData, testLabel);
-% fprintf("model loss rate is: %.4f \n", mdlLoss);
-% figure;
-% confusionchart(testLabel, mdlPred);
-% title("neural network");
-% 
-% %% svm
-% fprintf("svm \n")
-% mdl = fitcsvm(trainData, trainLabel,"OptimizeHyperparameters","auto");
-% 
-% mdlPred = string(predict(mdl, testData));
-% mdlLoss = loss(mdl, testData, testLabel);
-% fprintf("model loss rate is: %.4f \n", mdlLoss);
-% figure;
-% confusionchart(testLabel, mdlPred);
-% title("svm");
-% 
-%% random forest
-fprintf("RF \n")
-t = templateTree("Reproducible", true, "Surrogate", "on");
-% t = templateTree('NumVariablesToSample','all',...
-%     'PredictorSelection','interaction-curvature','Surrogate','on');
-options = struct("UseParallel",true);
-% [Mdl,OptimizationResults] = fitcauto(trainData, trainLabel, "Learners","auto",...
-%     "HyperparameterOptimizationOptions",options);
-% mdl = fitcensemble(trainData, trainLabel, 'Method','Bag',...
-%     'NumLearningCycles',200, 'Learners',t);
-mdl = fitcensemble(trainData, trainLabel,"OptimizeHyperparameters", "auto",...
-    "HyperparameterOptimizationOptions",options, "Learners", t);
+% %SE 0.2668  0.2716 
+% t = templateTree("Reproducible", true, "Surrogate", "on", "MinLeafSize", 24);
+% mdl = fitcensemble(trainData, trainLabel,"Method", "Bag",...
+%      "Learners", t, "NumLearningCycles", 59);
 
 mdlPred = string(predict(mdl, testData));
 mdlLoss = loss(mdl, testData, testLabel);
 fprintf("model loss rate is: %.4f \n", mdlLoss);
-figure;
+f1 = figure;
 confusionchart(testLabel, mdlPred);
-title("rf");
+set(f1,'Position',[10 10 350 300])
+fontsize(gcf,scale=1.2)
+% exportgraphics(f1, 'print/SWconfusion.png', 'Resolution',300);
 
 impOOB = oobPermutedPredictorImportance(mdl);
-figure;
-bar(impOOB)
-title('Unbiased Predictor Importance Estimates')
-xlabel('Predictor variable')
+f2 = figure;
+b = bar(impOOB,'FaceColor','flat');
+% b.CData = [0 0.4470 0.7410
+%            0.8500 0.3250 0.0980
+%            0.9290 0.6940 0.1250
+%            0.4940 0.1840 0.5560
+%            0.3010 0.7450 0.9330];
+% title('Unbiased Predictor Importance Estimates')
+% xlabel('Predictors')
 ylabel('Importance')
 h = gca;
 h.XTickLabel = mdl.PredictorNames;
-% h.XTickLabelRotation = 45;
-% h.TickLabelInterpreter = 'none';
+grid on
+fontsize(gcf,scale=1.2)
+% exportgraphics(f2, 'print/SWimportance.png', 'Resolution',300);
 
-yHat = oobPredict(mdl);
-R2 = corr(mdl.Y, yHat)^2
+f3 = figure;
+ax = gca();
+b = pie(ax, impOOB);
+% ax.Colormap = [0 0.4470 0.7410
+%                0.8500 0.3250 0.0980
+%                0.9290 0.6940 0.1250
+%                0.4940 0.1840 0.5560
+%                0.3010 0.7450 0.9330];
+set(f3,'Position',[10 10 350 300])
+fontsize(gcf,scale=1.2)
+% exportgraphics(f3, 'print/SWpie.png', 'Resolution',300);
