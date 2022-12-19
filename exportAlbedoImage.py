1e0444867a9953b5528cbae32a45e82c84d02ed1
fsn1995 <fsn.1995@gmail.com>
2022-12-19 09:04:12 +0100

    update elevhist plot

--- exportAlbedoImage.py
+++ exportAlbedoImage.py
@@ -21,7 +21,24 @@ date_start = ee.Date.fromYMD(2009, 8, 16)
 date_end = ee.Date.fromYMD(2017, 3, 12)
 
 
-aoi = ee.FeatureCollection("projects/ee-deeppurple/assets/icePoly")
+# aoi = ee.FeatureCollection("projects/ee-deeppurple/assets/icePoly")
+aoi = ee.Geometry.Polygon(
+    [[[-36.29516924635421, 83.70737243835941],
+    [-51.85180987135421, 82.75597137647488],
+    [-61.43188799635421, 81.99879137488564],
+    [-74.08813799635422, 78.10103528196419],
+    [-70.13305987135422, 75.65372336709613],
+    [-61.08032549635421, 75.71891096312955],
+    [-52.20337237135421, 60.9795530382023],
+    [-43.41430987135421, 58.59235996703347],
+    [-38.49243487135421, 64.70478286561182],
+    [-19.771731746354217, 69.72271161037442],
+    [-15.728762996354217, 76.0828635948066],
+    [-15.904544246354217, 79.45091003031243],
+    [-10.015872371354217, 81.62328742628017],
+    [-26.627200496354217, 83.43179828852398],
+    [-31.636966121354217, 83.7553561747887]]]
+) # whole greenland
 
 #%% prepare harmonized albedo 
 
@@ -91,7 +108,7 @@ def imRangeFilter(image):
   maskMin = image.gt(0)
   return image.updateMask(maskMax).updateMask(maskMin)
 def bareiceFilter(image):
-  maskMax = image.lt(0.65)
+  maskMax = image.lte(0.65)
   maskMin = image.gt(0)
   return image.updateMask(maskMax).updateMask(maskMin)
 '''
@@ -242,16 +259,16 @@ visParams = {
 albedoImage = multiSat.mean().updateMask(greenlandmask)
 # Map.addLayer(albedoImage, visParams, 'harmonized albedo')
 
-xMin = -73.297
-xMax = -12.20855
-yMin = 60.03676
-yMax = 83.64513
-region = ee.Geometry.BBox(west=xMin, south=yMin, east=xMax, north=yMax)
+# xMin = -73.297
+# xMax = -12.20855
+# yMin = 60.03676
+# yMax = 83.64513
+# region = ee.Geometry.BBox(west=xMin, south=yMin, east=xMax, north=yMax)
 task = ee.batch.Export.image.toAsset(
   image=albedoImage,
   description="albedoArcticDEM",
   assetId="projects/ee-deeppurple/assets/topography/albedoArcticDEM",
-  region=region,
+  region=aoi,
   scale=30,
   maxPixels=1e13
 )
